35ed3f3961e71f192d63d4f0a6efd262e208f794
Yazhou He <hyz2993@gmail.com>
2022-09-02 13:13:38 +0100

    add mediainfo for mkv files in mediainfo_av_cmdline_mac.sh

--- mediainfo_av_cmdline_mac.sh
+++ mediainfo_av_cmdline_mac.sh
@@ -1,5 +1,8 @@
 ## mediainfo wav
 for file in */*x*/; do echo "$file" && mediainfo "$file"*.wav>>mediainfo_a.txt; done
 
-## mediainfo image
-for file in */*x*/; do echo "$file" && mediainfo "$file"*6400.dpx>>mediainfo_v.txt; done
+## mediainfo dpx image
+for file in */*x*/; do echo "$file" && mediainfo "$file"*6400.dpx>>mediainfo_dpx.txt; done
+
+## mediainfo mkv (SIPped, could take a while as the file will be large)
+for file in oe*/*/objects/; do echo "$file" && mediainfo "$file"*.mkv>>mediainfo_mkv.txt; done
