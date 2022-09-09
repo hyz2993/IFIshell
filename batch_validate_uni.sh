c221d4fb81efc9d861376d6c8d6f6f11de52a58c
Yazhou He <hyz2993@gmail.com>
2022-09-09 16:20:37 +0100

    remove [10%] bar from val_result.txt

--- batch_validate_uni.sh
+++ batch_validate_uni.sh
@@ -9,12 +9,12 @@ pwd
 local_path=$(pwd)
 sleep 0.5
 
-echo "This shell script is written by Yazhou He./n
+echo "This shell script is written by Yazhou He.
 		It's a universal version for dpx&wav validation.
 		The DPX folder is now named Scan;
 		The WAV folder should be named WAV"
 
-	echo -e "---\ncheck wav md5"
+	echo -e "---\n\ncheck wav md5"
 	sleep 2
 	for dir in */Wav/;
 	do  
@@ -31,7 +31,7 @@ echo "This shell script is written by Yazhou He./n
 
 
 
-	echo -e "---\nmove wav to image folder"
+	echo -e "---\n\nmove wav to image folder"
 	sleep 2
 	for dir in *;
 	do
@@ -42,7 +42,7 @@ echo "This shell script is written by Yazhou He./n
 
 
 
-	echo -e "---\nmove checksum to parent folder"
+	echo -e "---\n\nmove checksum to parent folder"
 	sleep 2
 	for dir in */;
 	do
@@ -68,7 +68,7 @@ echo "This shell script is written by Yazhou He./n
 
 
 
-	echo -e "---\nchange structure"
+	echo -e "---\n\nchange structure"
 	sleep 2
 	for dir in */;
 	do
@@ -84,64 +84,25 @@ echo "This shell script is written by Yazhou He./n
 
 
 
-	[15:47] Yazhou He
-	#! /bin/bash
-
-	# auto relocate - if disable here, use cd to relocate manually
-	#local_path=$(dirname $0)
-	#cd $local_path
-
-	# show the current path
-	pwd
-	local_path=$(pwd)
-	sleep 0.5
-
-	echo "This shell script is written by Yazhou He"
-
-
-	echo -e "---\ncheck wav md5"
-	sleep 2
-	for dir in */Wav/;do      echo "object $dir processing"     cat "$dir"*.md5    md5 "$dir"*.wav    echo "-";
-	done
-
-	echo -e "Go back and compare all audio checksums.\nPress any [key] and enter to continue"
-	read any_key
-
-	echo -e "---\nmove wav to image folder"
-	sleep 2
-	for dir in *;
-	do    echo "object $dir processing"    mv "$dir"Wav/*.wav "$dir"Scan/    echo "-";
-	done
-
-	echo -e "---\nmove checksum to parent folder"
-	sleep 2
-	for dir in */;
-	do    echo "object $dir processing"    mv "$dir"*/CHECKSUM.md5 "$dir"    echo "-";
-	done
-
-	#echo -e "---\nrename manifest"
-	#sleep 2
-	#for dir in */Scan/;
-	#do    #res1="${dir: -10}"    #res="${res1::9}"    #path="${dir%%$res*}"    #echo "object $path processing"    #echo "resoluation: $res"    #mv "$path"CHECKSUM.md5 "$path""$res"_manifest.md5    #echo "-";
-	#done
-
-	echo -e "---\nchange structure"
-	sleep 2
-	for dir in */;
-	do    echo "object $dir processing"    filename="$dir"CHECKSUM.md5    #res1=${filename%%_manifest*}    #res=${res1: -9}    echo "$filename has found generating copy"    cat $filename | tr "[:upper:]" "[:lower:]" > "$dir"CHECKSUM2.md5    sed -i "" 's/\*/ Scan\//' "$dir"CHECKSUM2.md5    echo "-";
-	done
-
-	echo -e "---\nvalidation"
+	echo -e "---\n\nvalidation"
 	sleep 2
 	for dir in */;
 	do
 		echo "-"
 		filename="$dir"CHECKSUM2.md5
-		echo "validating for $filename"
+		echo "validating $filename"
 		target_md5=$local_path/$filename
-		#target_md5=${target_md5// /\\ }
+		target_md5=${target_md5// /\\ }
 		echo "Target: $target_md5" > "$local_path"/"$dir"/val_result.txt
 		validate.py -y "$target_md5" >> "$local_path"/"$dir"/val_result.txt
-		# not finished yet, trying to remove the "[xx%]" in the text file
-		#sed -i "" '/%/d' "$local_path"/"$dir"/val_result.txt
+        tr '\r' '\n' > "$dir"val_result.txt
+		sed -i "" "/%/d" "$dir"val_result.txt
+        echo "checking $dirval_result.txt"
+        grep -c "All checksums have validated" "$dir"val_result.txt
+		if [ `grep -c "All checksums have validated" "$dir"val_result.txt` -ne '0' ]
+		    then
+		        echo "$dir All checksums validated"
+		    else
+		        echo "ERROR: [CHECK THE RESULT TEXT FILE]"
+		    fi
 	done
